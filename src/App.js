39190a32eb9e7bca223258559c23655a33d3913b
bugzzbunny007 <harshyadav94250@gmail.com>
2021-01-02 22:51:50 +0530

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import logo from './assets/img/logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import arrow from './assets/img/arrow.svg';
+import profile from './assets/img/profile.svg';
+
+import {BrowserRouter as Router,Switch,Route,Link} from 'react-router-dom';
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <nav className="navbar shadow-sm navbar-expand-lg navbar-light bg-white p-0">
+        <div className="container-fluid p-0">
+          <Link className="pl-5 navbar-brand" to="#"> 
+            <img src={logo} alt="logo"/>
+            <b>Intugine</b>
+          </Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item  bg-color-nav">
+                <Link className="nav-link active p-2" aria-current="page" to="#">Home</Link>
+              </li>
+              <li className="nav-item  bg-color-nav">
+                <Link className="nav-link p-2" to="#">Brands</Link>
+              </li>
+              <li className="nav-item  bg-color-nav">
+                <Link className="nav-link p-2" to="#" tabindex="-1" >Transporters </Link>
+              </li>
+              <li className="nav-item bg-color-nav">
+                <Link className="nav-link p-2" to="#" tabindex="-1" ><img height={36} className="circle p-2" src={profile} alt="profile"/></Link>
+              </li>
+              <li className="nav-item  bg-color-nav">
+                <Link className="nav-link text-center pt-2" to="#"><img className="p-2" src={arrow} alt="arrow"/></Link></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      
+
     </div>
-  );
+    </Router>
+      );
 }
 
 export default App;
---
+++ src/card.js
@@ -0,0 +1,20 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Card() {
+  return (
+    <div className="">
+       
+      <div className=" d-flex justify-content-center">
+        <div style={{backgroundColor: '#6B6DFB'}} className="card m-2">
+          <p>DEL</p> </div>
+        <div style={{backgroundColor: '#6B6DFB'}} className="card m-2"></div>
+        <div style={{backgroundColor: '#6B6DFB'}} className="card m-2"></div>
+        <div style={{backgroundColor: '#6B6DFB'}} className="card m-2"></div>
+        <div style={{backgroundColor: '#6B6DFB'}} className="card m-2"></div>
+      </div>
+     </div>
+    );
+}
+
+export default Card;
